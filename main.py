cbf39cb1f631c9171998640935d1bd577be78853
Lukasz <lukasz.bielak1@gmail.com>
2022-06-02 19:54:32 +0200

    Refactor

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     spark = SparkSession\
             .builder\
             .master('local[*]')\
-            .config('spark.jars', '/Users/lukaszbielak/spark/jars/mssql-jdbc-10.2.1.jre8.jar')\
+            .config('spark.jars', '/Users//spark/jars/mssql-jdbc-10.2.1.jre8.jar')\
             .config('spark.executor.extraClassPath', '/Users//spark/jars/mssql-jdbc-10.2.1.jre8.jar')\
             .appName("PySpark")\
             .getOrCreate()
